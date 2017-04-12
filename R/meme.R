4524c7f3b50242fc3bcd80fc805329bc7e5276b3
Diego Diez <diego10ruiz@gmail.com>
2017-04-12 17:14:50 +0900

    Print help when `help = TRUE` or `args` contains `-h`.

--- R/meme.R
+++ R/meme.R
@@ -16,36 +16,51 @@ meme <- R6::R6Class("meme",
       system2(self$dockerbin, args)
     },
 
-    meme = function(dataset = NULL, args = NULL, outdir = "meme_out", force_clean = TRUE) {
-      if (is.null(dataset)) stop("argument 'dataset' is required.")
+    meme = function(dataset = NULL, args = NULL, outdir = "meme_out", force_clean = TRUE, help = FALSE) {
+      if (help || grepl("-h", args)) {
+        args <- paste("run", self$image, "meme", "-h")
+        system2(self$dockerbin, args)
+      } else {
+        if (is.null(dataset)) stop("argument 'dataset' is required.")
 
-      if (force_clean)
-        args <- paste("-oc", outdir, args)
-      else
-        args <- paste("-o", outdir, args)
+        if (force_clean)
+          args <- paste("-oc", outdir, args)
+        else
+          args <- paste("-o", outdir, args)
 
-      args <- paste("run", "-v", self$voldir, self$image, "meme", dataset, args)
-      system2(self$dockerbin, args)
+        args <- paste("run", "-v", self$voldir, self$image, "meme", dataset, args)
+        system2(self$dockerbin, args)
+      }
     },
 
-    fimo = function(motif = NULL, sequence = NULL, args = NULL, outdir = "fimo_out", force_clean = TRUE) {
-      if (force_clean)
-        args <- paste("-oc", outdir, args)
-      else
-        args <- paste("-o", outdir, args)
+    fimo = function(motif = NULL, sequence = NULL, args = NULL, outdir = "fimo_out", force_clean = TRUE, help = FALSE) {
+      if (help || grepl("-h", args)) {
+        args <- paste("run", self$image, "fimo", "-h")
+        system2(self$dockerbin, args)
+      } else {
+        if (force_clean)
+          args <- paste("-oc", outdir, args)
+        else
+          args <- paste("-o", outdir, args)
 
-      args <- paste("run", "-v", self$voldir, self$image, "fimo", args, motif, sequence)#, "2>", logfile)
-      system2(self$dockerbin, args)
+        args <- paste("run", "-v", self$voldir, self$image, "fimo", args, motif, sequence)#, "2>", logfile)
+        system2(self$dockerbin, args)
+      }
     },
 
-    tomtom = function(query = NULL, target = NULL, args = NULL, outdir = "tomtom_out", force_clean = TRUE) {
-      if (force_clean)
-        args <- paste("-oc", outdir, args)
-      else
-        args <- paste("-o", outdir, args)
+    tomtom = function(query = NULL, target = NULL, args = NULL, outdir = "tomtom_out", force_clean = TRUE, help = FALSE) {
+      if (help || grepl("-h", args)) {
+        args <- paste("run", self$image, "tomtom", "-h")
+        system2(self$dockerbin, args)
+      } else {
+        if (force_clean)
+          args <- paste("-oc", outdir, args)
+        else
+          args <- paste("-o", outdir, args)
 
-      args <- paste("run", "-v", self$voldir, self$image, "tomtom", args, query, target)
-      system2(self$dockerbin, args)
+        args <- paste("run", "-v", self$voldir, self$image, "tomtom", args, query, target)
+        system2(self$dockerbin, args)
+      }
     }
 
   ))
